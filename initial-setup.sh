ad4790ac495695d7a5e0520d34a3af4023b415eb
Sanjay Kasi <78728234+skasi-surya@users.noreply.github.com>
2022-05-09 09:28:16 +0530

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -9,45 +9,6 @@ printf "\n\nInstall Git...START"
 sudo apt install build-essential unzip curl git git-lfs -y
 printf "\n\nInstall Git...COMPLETE"
 
-# Install Java 11
-printf "\n\nInstall Java...START"
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 11.0.15-tem
-source ~/.bashrc
-# Check Java installation
-checkJava="java --version"
-if eval $checkJava | grep -q '11.0.15'; then
-    printf "\n\nInstall Java...COMPLETE"
-else
-    printf "\n\nInstall Java...FAILED"
-fi
-
-# Install Docker
-printf "\n\nInstall Docker...START"
-sudo apt install apt-transport-https ca-certificates software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt install docker-ce -y
-# check if service is running - sudo systemctl status docker
-checkDocker="sudo systemctl status docker"
-if eval $checkDocker | grep -q 'active'; then
-    sudo usermod -aG docker ${USER}
-    printf "\n\nInstall Docker...COMPLETE"
-else
-    printf "\n\nInstall Docker...FAILED"
-fi
-
-printf "\n\nInstall docker-compose...START"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-checkDockerCompose="docker-compose --version"
-sudo chmod +x /usr/local/bin/docker-compose
-if eval $checkDockerCompose | grep -q '1.29.2'; then
-    printf "\n\nInstall Docker-Compose...COMPLETE"
-else
-    printf "\n\nInstall Docker-Compose...FAILED"
-fi
-
 printf "\n\nInstall Postman...START"
 sudo snap install postman
 printf "\n\nInstall Postman...COMPLETE"
@@ -77,3 +38,12 @@ printf "\n\nDONE!!"
 printf "\n\nPlease enter password to simulate logout:"
 
 su - ${USER}
+
+# Install SDKMan, Java 11 manually
+# https://sdkman.io/install
+# sdk list java
+# sdk java install 11.0.15-tem
+
+# Install docker-ce and docker compose manually
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-20-04
